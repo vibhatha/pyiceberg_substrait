ac5ed99041f2984f2843d526cce1f85443f20c38
vibhatha <vibhatha@gmail.com>
2023-05-10 16:11:57 +0530

    feat: adding visitor pattern

--- src/icetrait/iceberg/process.py
+++ src/icetrait/iceberg/process.py
@@ -104,3 +104,84 @@ class ProcessSubstrait:
                 raise ValueError(f"Unsupported file format {file_format}")
             local_files.items.append(file_or_files)
         read_rel.local_files.CopyFrom(local_files)
+
+    
+    def visitor_help(self):
+        if self._substrait_plan.relations:
+            relations = self.plan.relations
+            if relations:
+                if relations[0].HasField("root"):
+                    rel_root = relations[0].root
+                    print("RelRoot : ", type(rel_root))
+                    rel = rel_root.input
+                    return self.visit_rel(rel)
+        return None
+
+   
+    def visit_rel(self, rel):
+        if rel:
+            if rel.HasField("aggregate"):
+                aggregate = rel.aggregate
+                if aggregate.HasField("input"):
+                    input = aggregate.input
+                    print(type(aggregate), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("cross"):
+                cross = rel.cross
+                if cross.HasField("input"):
+                    input = cross.input
+                    print(type(cross), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("fetch"):
+                fetch = rel.fetch
+                if fetch.HasField("input"):
+                    input = fetch.input
+                    print(type(fetch), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("filter"):
+                filter = rel.filter
+                if filter.HasField("input"):
+                    input = filter.input
+                    print(type(filter), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("hash_join"):
+                hash_join = rel.hash_join
+                if hash_join.HasField("input"):
+                    input = hash_join.input
+                    print(type(hash_join), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("join"):
+                join = rel.join
+                if join.HasField("input"):
+                    input = join.input
+                    print(type(join), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("merge_join"):
+                merge_join = rel.merge_join
+                if merge_join.HasField("input"):
+                    input = merge_join.input
+                    print(type(merge_join), type(input))
+                    return self.visit_rel(input)
+            if rel.HasField("project"):
+                project = rel.project
+                if project.HasField("input"):
+                    input = project.input
+                    print(type(project), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("read"):
+                return rel.read
+            elif rel.HasField("set"):
+                set = rel.set
+                if set.HasField("input"):
+                    input = set.input
+                    print(type(set), type(input))
+                    return self.visit_rel(input)
+            elif rel.HasField("sort"):
+                sort = rel.sort
+                if sort.HasField("input"):
+                    input = sort.input
+                    print(type(sort), type(input))
+                    return self.visit_rel(input)
+            else:
+                raise Exception("Invalid relation!")
+        return None
--- tests/test_import.py
+++ tests/test_import.py
@@ -2,6 +2,7 @@ import ibis
 from ibis_substrait.compiler.core import SubstraitCompiler
 from google.protobuf import json_format
 from google.protobuf.json_format import MessageToJson
+from substrait.gen.proto.algebra_pb2 import AggregateRel, CrossRel, FetchRel, FilterRel, HashJoinRel, JoinRel, MergeJoinRel, ProjectRel, ReadRel, SetRel, SortRel
 
 from icetrait.iceberg.process import ProcessSubstrait
 
@@ -22,6 +23,73 @@ def test_substrait_basic():
     local_files = read_rel.local_files
     for id, file in enumerate(local_files.items):
         assert files[id] == file.uri_file
-        # TODO: figure out a way to validate file.format update
+        # TODO: https://github.com/vibhatha/pyiceberg_substrait/issues/3
+        
+
+def test_visitor():
+    tb = ibis.table([("id", "date"), ("bid", "float"), ("ask", "int32"), ("symbol", "int64")], "t",)
+    query = tb.select(["id", "bid", "ask", "symbol"])
+    compiler = SubstraitCompiler()
+    protobuf_msg = compiler.compile(query).SerializeToString()
+
+    from icetrait.substrait.visitor import RelVisitor, SubstraitPlanEditor, visit_and_update, RelUpdateVisitor
+    
+    class RelValidateVisitor(RelVisitor):
+        
+        def __init__(self, files, formats):
+            self._files = files
+            self._formats = formats
+        
+        def visit_aggregate(self, rel: AggregateRel):
+            pass
+        
+        def visit_cross(self, rel: CrossRel):
+            pass
+        
+        def visit_fetch(self, rel: FetchRel):
+            pass
+        
+        def visit_filter(self, rel: FilterRel):
+            pass
+        
+        def visit_join(self, rel: JoinRel):
+            pass
+        
+        def visit_hashjoin(self, rel: HashJoinRel):
+            pass
+        
+        def visit_merge(self, rel: MergeJoinRel):
+            pass
+        
+        def visit_project(self, rel: ProjectRel):
+            pass
+        
+        def visit_read(self, read_rel: ReadRel):
+            local_files = read_rel.local_files
+            for id, file in enumerate(local_files.items):
+                assert self._files[id] == file.uri_file
+                # TODO: https://github.com/vibhatha/pyiceberg_substrait/issues/3
+        
+        def visit_set(self, rel: SetRel):
+            pass
+        
+        def visit_sort(self, rel: SortRel):
+            pass
+    
+    
+    editor = SubstraitPlanEditor(protobuf_msg)
+    print("-" * 80)
+    print(editor.rel)
+    files = ["s3://data/p1.parquet", "s3://data/p2.orc", "s3://data/p3.dwrf", "s3://data/p4.arrow"]
+    file_formats = ["parquet", "orc", "dwrf", "orc"]
+    update_visitor = RelUpdateVisitor(files=files, formats=file_formats)
+    visit_and_update(editor.rel, update_visitor)
+    
+    print(editor.plan)
+    
+    validate_visitor = RelValidateVisitor(files=files, formats=file_formats)
+    visit_and_update(editor.rel, validate_visitor)
+    
+    
     
         
