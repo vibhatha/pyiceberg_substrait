2fe6863a7b1e558a72620b019a8e08634f9eb938
vibhatha <vibhatha@gmail.com>
2023-05-12 11:23:54 +0530

    feat: minor changes to wrapper

--- src/icetrait/duckdb/wrapper.py
+++ src/icetrait/duckdb/wrapper.py
@@ -33,7 +33,6 @@ class DuckdbSubstrait:
         self._con = None
         self._initialize()
         self._get_table_name()
-        self._update_with_local_file_paths()
         
     @property
     def plan(self):
@@ -58,24 +57,40 @@ class DuckdbSubstrait:
         rel = extract_rel_from_plan(self._plan)
         visit_and_update(rel, extract_visitor)
         return extract_visitor.table_names[0]
-    
-    def _update_with_local_file_paths(self):
+
+    def update_schema(self):
+        table_name = self.table_name
+        full_table_name = f"{self._duckdb_schema}.{table_name}"
+        editor = SubstraitPlanEditor(self._plan)
+        named_table_update_visitor = NamedTableUpdateVisitor(full_table_name)
+        visit_and_update(editor.rel, named_table_update_visitor)
+        self._updated_plan = editor.plan
+
+    def update_with_local_file_paths(self):
         # this method would update the passed Substrait plan
         # with s3 urls to local files 
         # Issue: https://github.com/duckdb/duckdb/discussions/7252
         table_name = self.table_name
         downloader = IcebergFileDownloader(catalog=self._catalog_name, table=table_name, local_path=self._local_path)
         self._files, self._formats = downloader.download()
-        editor = SubstraitPlanEditor(self._plan)
-        full_table_name = f"{self._duckdb_schema}.{table_name}"
-        named_table_update_visitor = NamedTableUpdateVisitor(full_table_name)
-        visit_and_update(editor.rel, named_table_update_visitor)
         update_visitor = RelUpdateVisitor(files=self._files, formats=self._formats)
+        editor = SubstraitPlanEditor(self._updated_plan.SerializeToString())
         visit_and_update(editor.rel, update_visitor)
         self._updated_plan = editor.plan
+
+    @property
+    def updated_plan(self):
+        return self._updated_plan
     
     def execute(self):
         # run the updated Substrait plan with DuckDb
         proto_bytes = self._updated_plan.SerializeToString()
         query_result = self._con.from_substrait(proto=proto_bytes)
         return query_result
+
+
+def run_query(plan: SubstraitPlan, catalog_name:str, local_path:str, duckdb_schema:str):
+    wrapper = DuckdbSubstrait(plan=plan, catalog_name=catalog_name, local_path=local_path, duckdb_schema=duckdb_schema)
+    wrapper.update_schema()
+    wrapper.update_with_local_file_paths()
+    return wrapper.execute()
